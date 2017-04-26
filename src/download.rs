96436d966677af844bfe83c468dfe859509274ce
Jeremy Soller <jackpot51@gmail.com>
2017-04-25 21:44:27 -0600

    res -> response

--- src/download.rs
+++ src/download.rs
@@ -18,16 +18,16 @@ pub fn download(remote_path: &str, local_path: &str) -> io::Result<()> {
     let mut client = Client::with_connector(HttpsConnector::new(TlsClient::new()));
     client.set_read_timeout(Some(Duration::new(5, 0)));
     client.set_write_timeout(Some(Duration::new(5, 0)));
-    let mut res = match client.get(remote_path).send() {
-        Ok(res) => res,
+    let mut response = match client.get(remote_path).send() {
+        Ok(response) => response,
         Err(HyperError::Io(err)) => return Err(err),
         Err(err) => return Err(io::Error::new(io::ErrorKind::Other, err.description()))
     };
 
-    match res.status {
+    match response.status {
         StatusCode::Ok => {
             let mut count = 0;
-            let length = res.headers.get::<ContentLength>().map_or(0, |h| h.0 as usize);
+            let length = response.headers.get::<ContentLength>().map_or(0, |h| h.0 as usize);
 
             let mut status = [b' '; 50];
 
@@ -63,7 +63,7 @@ pub fn download(remote_path: &str, local_path: &str) -> io::Result<()> {
                 let _ = write!(stderr, "] {:>4} {}", size, suffix);
 
                 let mut buf = [0; 8192];
-                let res = res.read(&mut buf)?;
+                let res = response.read(&mut buf)?;
                 if res == 0 {
                     break;
                 }
@@ -76,7 +76,7 @@ pub fn download(remote_path: &str, local_path: &str) -> io::Result<()> {
             Ok(())
         },
         _ => {
-            let _ = write!(stderr, "* Failure {}\n", res.status);
+            let _ = write!(stderr, "* Failure {}\n", response.status);
 
             Err(io::Error::new(io::ErrorKind::NotFound, format!("{} not found", remote_path)))
         }
