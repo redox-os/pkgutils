b1f80815e00780b69ebc48c3facb8eb8c4d77234
Wildan M <willnode@wellosoft.net>
2025-06-08 14:21:00 +0700

    Add install tests

---
+++ pkg-lib/tests/integration_test.rs
@@ -0,0 +1,37 @@
+use std::{cell::RefCell, rc::Rc};
+
+#[cfg(feature = "indicatif")]
+use pkg::callback::IndicatifCallback;
+
+#[cfg(feature = "indicatif")]
+use pkg::{Library, PackageName};
+
+#[test]
+#[cfg(feature = "indicatif")]
+fn test_pkg_install() -> Result<(), Box<dyn std::error::Error>> {
+    use std::fs;
+
+    let callback = IndicatifCallback::new();
+
+    let tmp_dir = std::env::current_dir()?.join("tests/staging");
+
+    // Simulate redox OS pkg library
+    fs::remove_dir_all(&tmp_dir)?;
+    let pkg_d_dir = tmp_dir.join("etc/pkg.d");
+    fs::create_dir_all(&pkg_d_dir)?;
+    let source_file = pkg_d_dir.join("50_redox");
+    fs::write(&source_file, "https://static.redox-os.org/pkg")?;
+
+    fs::create_dir_all(&tmp_dir)?;
+
+    let mut library = Library::new(
+        tmp_dir,
+        "x86_64-unknown-redox",
+        Rc::new(RefCell::new(callback)),
+    )?;
+
+    let list = vec![PackageName::new("bootloader")?];
+    library.install(list)?;
+    library.apply()?;
+    Ok(())
+}
